39e87e5d191aa3839f6c9d961586e794890c3806
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-03-28 14:48:08 +0100

    add assets/ and GameTheme start

---
+++ include/GameTheme.hpp
@@ -0,0 +1,12 @@
+// Filename: GameTheme.hpp
+// Author: Lucian Mocan
+// Date: 28-03-2025
+// Description: 
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+struct GameTheme {
+    sf::Color backgroundColor;
+}
