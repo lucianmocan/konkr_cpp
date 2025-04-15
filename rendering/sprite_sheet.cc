6803bc3907882f20248e86158a330d817419d63b
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-04-15 20:52:36 +0200

    start setting up sprite sheet config

---
+++ rendering/sprite_sheet.cc
@@ -0,0 +1,11 @@
+// Copyright 2025 Lucian Mocan, Antoine Waehren. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+//
+// sprite_sheet.cc
+//
+// Implements the SpriteSheet class for a Konkr.io clone, handling the loading
+// and rendering of sprite sheets, as well as the management of individual sprites.
+//
+#include "rendering/sprite_sheet.h"
+
+#include <SFML/Graphics.hpp>
---
+++ rendering/sprite_sheet.h
@@ -0,0 +1,24 @@
+// Copyright 2025 Lucian Mocan, Antoine Waehren. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+//
+// sprite_sheet.h
+//
+// Declares the SpriteSheet class and SpriteInfo struct for a Konkr.io clone,
+// managing sprite positions and dimensions from a spritesheet for rendering.
+//
+
+#ifndef KONKR_RENDERING_SPRITE_SHEET_H
+#define KONKR_RENDERING_SPRITE_SHEET_H
+
+#include <string>
+#include <unordered_map>
+
+namespace konkr {
+
+}
+
+struct SpriteInfo {
+    sf::IntRect rect;
+}
+
+#endif
