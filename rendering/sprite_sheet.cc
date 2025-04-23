399d28012c9764d659bd3f33e954a723c5b842bd
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-04-24 01:04:00 +0200

    looks good for implementation (but how do we add sprite defintions) ?

--- rendering/sprite_sheet.cc
+++ rendering/sprite_sheet.cc
@@ -8,4 +8,54 @@
 //
 #include "rendering/sprite_sheet.h"
 
+#include <stdexcept>
+#include <string>
+#include <optional>
+#include <utility>
+
 #include <SFML/Graphics.hpp>
+
+namespace konkr {
+
+
+bool SpriteSheet::loadFromFile(const std::filesystem::path& file_path) {
+    if (!texture_.loadFromFile(file_path.string())) {
+        return false;
+    }
+    loaded_ = true;
+    return true;
+}
+
+void SpriteSheet::addSprite(const std::string& name, const SpriteInfo& sprite_info) {
+    sprites_map_.emplace(name, sprite_info);
+}
+
+std::optional<SpriteInfo> SpriteSheet::getSpriteInfo(const std::string& name) const {
+    auto it = sprites_map_.find(name);
+    if (it != sprites_map_.end()) {
+        return it->second;
+    }
+    return std::nullopt;
+}
+
+std::optional<sf::Sprite> SpriteSheet::createSprite(const std::string& name) const {
+    if (!loaded_) {
+        return std::nullopt;
+    }
+    // if the sprite is not found, then there's a mistake somewhere in the calling function
+    std::optional<SpriteInfo> sprite_info = getSpriteInfo(name);
+    if (!sprite_info) {
+        return std::nullopt;
+    }
+    return sf::Sprite(texture_, sprite_info->rect);
+}
+
+const sf::Texture& SpriteSheet::getTexture() const {
+    if (!loaded_) {
+        throw std::runtime_error("SpriteSheet::getTexture() called before loading the texture.");
+    }
+    return texture_;
+}
+
+
+} // namespace konkr
