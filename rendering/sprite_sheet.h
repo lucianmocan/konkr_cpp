9c5dd70afce9b3708c04da21750a2f529e85ca26
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-04-24 00:53:11 +0200

    basic class to work with the SpriteSheet. (will definitely evolve!)

--- rendering/sprite_sheet.h
+++ rendering/sprite_sheet.h
@@ -13,12 +13,31 @@
 #include <string>
 #include <unordered_map>
 
-namespace konkr {
+#include <SFML/Graphics.hpp>
 
-}
+namespace konkr {
 
 struct SpriteInfo {
     sf::IntRect rect;
-}
+};
+
+class SpriteSheet {
+    public:
+
+        bool loadFromFile(const std::filesystem::path& file_path);
+        void addSprite(const std::string& name, const SpriteInfo& sprite_info);
+
+        std::optional<SpriteInfo> getSpriteInfo(const std::string& name) const;
+        std::optional<sf::Sprite> createSprite(const std::string& name) const;
+
+        const sf::Texture& getTexture() const;
+
+    private:
+        sf::Texture texture_;
+        std::unordered_map<std::string, SpriteInfo> sprites_map_;
+        bool loaded_ = false;
+};
+
+} // namespace konkr
 
 #endif
