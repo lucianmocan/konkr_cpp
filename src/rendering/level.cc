d32efa803eae53e77a7810772fb9eee253875b91
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-04-25 18:28:02 +0200

    modify the way levels are loaded. create a list of all available levels and load file contents on demand

--- src/rendering/level.cc
+++ src/rendering/level.cc
@@ -1,45 +1,69 @@
 // Copyright 2025 Lucian Mocan, Antoine Waehren. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for
-// details.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for details.
 
 #include "rendering/level.h"
 
+#include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <optional>
 
 namespace konkr {
 
-std::optional<Level> Level::LoadFromFile(const std::string& file_path) {
+// Level names are in snake_case, we want to display them in a more
+// human-readable format. Replaces underscores with spaces and capitalizes the
+// first letter.
+std::string FormatLevelNameForDisplay(std::string name) {
+  std::replace(name.begin(), name.end(), '_', ' ');
+  name[0] = std::toupper(name[0]);
+  return name;
+}
 
-    std::ifstream definition_stream(file_path);
-    if (!definition_stream.is_open()) {
-      std::cerr << "Failed to open level file: "
-                << file_path<< std::endl;
-      return std::nullopt;
+std::vector<Level> Level::GetAvailableLevels(
+    const std::filesystem::path& levels_directory) {
+  std::vector<Level> levels;
+
+  for (const auto& entry :
+       std::filesystem::recursive_directory_iterator(levels_directory)) {
+    if (entry.is_regular_file() && entry.path().extension() == ".level") {
+      std::string filename = entry.path().stem().string();
+      std::string category = entry.path().parent_path().filename().string();
+      std::string formatted_name = FormatLevelNameForDisplay(filename);
+
+      levels.emplace_back(formatted_name, category, entry.path());
     }
-
-    Level level;
-    std::string line;
-    while (std::getline(definition_stream, line)) {
-      if (level.name_.empty()) {
-        level.name_ = line;  // First non-empty line is the level name
-      } else {
-        level.map_.push_back(line);  // The rest are part of the map
-      }
-    }
-
-    if (level.map_.empty()) {
-      std::cerr << "No map data found in level file: " << file_path << std::endl;
-      return std::nullopt;
-    }
-
-    return level;
   }
 
-  void Level::DisplayMapAscii() const {
-    for (const auto& line : map_) {
-      std::cout << line << std::endl;
-    }
+  return levels;
+}
+
+bool Level::Load() {
+  std::ifstream definition_stream(file_path_);
+  if (!definition_stream.is_open()) {
+    std::cerr << "Failed to open level file: " << file_path_ << std::endl;
+    return false;
+  }
+
+  std::string line;
+  while (std::getline(definition_stream, line)) {
+    map_.push_back(line);
+  }
+
+  if (map_.empty()) {
+    std::cerr << "No map data found in level file: " << file_path_ << std::endl;
+    return false;
+  }
+
+  loaded_ = true;
+  return true;
+}
+
+void Level::DisplayMapAscii() const {
+  for (const auto& line : map_) {
+    std::cout << line << std::endl;
   }
 }
+
+}  // namespace konkr
--- src/rendering/level.h
+++ src/rendering/level.h
@@ -1,34 +1,56 @@
 // Copyright 2025 Lucian Mocan, Antoine Waehren. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for
-// details.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for details.
 //
 // level.h
 //
-// Declares the Level class 
+// Declares the Level class
 //
 
 #ifndef KONKR_RENDERING_LEVEL_H
 #define KONKR_RENDERING_LEVEL_H
 
+#include <filesystem>
+#include <optional>
 #include <string>
 #include <vector>
-#include <optional>
 
 namespace konkr {
 
 class Level {
  public:
-  // Returns a level if the file is successfully loaded
-  static std::optional<Level> LoadFromFile(const std::string& file_path);
+  // Creates Level objects for all levels found in the specified directory.
+  // The directory should contain subdirectories for each category, and each
+  // level file should have a ".level" extension.
+  // The level name is derived from the file name, and the category is derived
+  // from the directory name.
+  // It doesn't load the contents of the level files, just prepares the Level
+  // objects.
+  static std::vector<Level> GetAvailableLevels(
+      const std::filesystem::path& levels_directory);
+
+  Level(std::string name, std::string category, std::filesystem::path file_path)
+      : name_(std::move(name)),
+        category_(std::move(category)),
+        file_path_(std::move(file_path)) {};
+
+  // Loads the level from the file_path_ if not already loaded
+  bool Load();
 
   const std::string& name() const { return name_; }
+  const std::string& category() const { return category_; }
+  const std::filesystem::path& file_path() const { return file_path_; }
   const std::vector<std::string>& map() const { return map_; }
+  bool isLoaded() const { return loaded_; }
 
   void DisplayMapAscii() const;
-  
+
  private:
   std::string name_;
+  std::string category_;
+  std::filesystem::path file_path_;
   std::vector<std::string> map_;
+  bool loaded_ = false;
 };
 
 }  // namespace konkr
