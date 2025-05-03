4962512420cca9061f50fc46c38339bdf2b33875
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2025-05-03 14:45:48 +0200

    update game_logic CMake and fix game.cc include + add ifndefs in game.h

--- src/game_logic/game.cc
+++ src/game_logic/game.cc
@@ -2,43 +2,10 @@
 // Licensed under the MIT License.
 // See LICENSE file in the project root for details.
 
+#include "game_logic/game.h"
+
 namespace konkr {
 
-Game::CheckEnd() {
-  const auto& tiles = level->tiles();
-  if (tiles.empty()) return;
+bool Game::CheckEnd() { return true; }
 
-  // We want to center the map in the window
-  // Calculate the map size
-  const size_t num_rows = tiles.size();
-  const size_t num_cols = tiles[0].size();
-  const float map_width =
-      num_cols * hex_width + (hex_width / 2);
-  const float map_height = num_rows * vert_spacing;
-
-  // Then we get the window size
-  const sf::Vector2u window_size = target.getSize();
-  const float window_width = static_cast<float>(window_size.x);
-  const float window_height = static_cast<float>(window_size.y);
-
-  // And we calculate the offsets (so that the map is centered)
-  const float x_offset = (window_width - map_width) / 2.0f;
-  const float y_offset = (window_height - map_height) / 2.0f;
-
-  for (size_t row = 0; row < tiles.size(); ++row) {
-    const auto& tile_row = tiles[row];
-    bool indent = !level->map()[row].empty() && level->map()[row][0] == '|';
-
-    for (size_t col = 0; col < tile_row.size(); ++col) {
-      const auto& tile_opt = tile_row[col];
-      if (!tile_opt) continue;
-
-      float x = col * hex_width + (indent ? (hex_width / 2) : 0) + x_padding +
-                x_offset;
-      float y = row * vert_spacing + y_padding + y_offset;
-      tile_opt->Render(target, sf::Vector2f(x, y), hex_radius, sprite_sheet);
-    }
-  }
-}
-
-}
+}  // namespace konkr
--- src/game_logic/game.h
+++ src/game_logic/game.h
@@ -2,21 +2,26 @@
 // Licensed under the MIT License.
 // See LICENSE file in the project root for details.
 
+#ifndef KONKR_GAME_LOGIC_GAME_H
+#define KONKR_GAME_LOGIC_GAME_H
+
 #include "rendering/level.h"
 
 namespace konkr {
 
 class Game {
-  public:
-    Game(int nb_players, std::shared_ptr<Level> cur_level)
-        : nb_players_(nb_players),
-          cur_level_(cur_level) {}
+ public:
+  Game(int nb_players, std::shared_ptr<Level> cur_level)
+      : nb_players_(nb_players), cur_level_(cur_level) {}
 
-    bool CheckEnd();
-  private:
-    int nb_players_;
-    int cur_player_ = 0;
-    std::shared_ptr<Level> cur_level_;
+  bool CheckEnd();
+
+ private:
+  int nb_players_;
+  int cur_player_ = 0;
+  std::shared_ptr<Level> cur_level_;
 };
 
-}
+}  // namespace konkr
+
+#endif  // KONKR_GAME_LOGIC_GAME_H
