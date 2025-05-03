e3c9e1c99d1c1b71b221247f08ec2aedd31b4f0e
antux18 <antux18@gmail.com>
2025-05-03 15:53:40 +0200

    Will be merged into class Level.

--- src/game_logic/game.h
+++ src/game_logic/game.h
@@ -6,20 +6,39 @@
 #define KONKR_GAME_LOGIC_GAME_H
 
 #include "rendering/level.h"
+#include "game_logic/player.h"
 
 namespace konkr {
 
 class Game {
- public:
-  Game(int nb_players, std::shared_ptr<Level> cur_level)
-      : nb_players_(nb_players), cur_level_(cur_level) {}
+  using PlayersList = std::vector<std::shared_ptr<Player>>;
 
-  bool CheckEnd();
+  public:
+    Game(std::shared_ptr<Level> level)
+        : level_(level) {
+          for (size_t i = 0; i < level_.CountActivePlayers(); i++) {
+            players_.push_back(std::make_shared<Player>(new Player(i)));
+          }
+        }
 
- private:
-  int nb_players_;
-  int cur_player_ = 0;
-  std::shared_ptr<Level> cur_level_;
+    Game(PlayersList players, LevelsList levels)
+    : players_(std::move(players)),
+      levels_(std::move(levels)) {}
+
+    inline const size_t getNumberPlayers() { return players_.size(); }
+
+    inline const std::shared_ptr<Player> getCurPlayer() { return players_[cur_player_idx_]; }
+
+    inline void nextTurn() { cur_player_idx_ = (cur_player_idx_+1) % getNumberPlayers(); };
+
+    void NextLevel();
+
+    const bool CheckEnd();
+
+  private:
+    std::shared_ptr<Level> level_;
+    size_t cur_player_idx_ = 0;
+    size_t cur_level_idx_ = 0;
 };
 
 }  // namespace konkr
